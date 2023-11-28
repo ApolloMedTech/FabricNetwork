521a7c77f439b5cac3ea18ed666cd314d42400da
Alexandre Monteiro <125086148+DarkMatterAlchemist@users.noreply.github.com>
2023-11-28 18:55:32 +0000

    Criar o executor.

---
+++ ChainCodes/Patient/Execute/patientManagement.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+// Método de start quando o chaincode leva deploy.
+func main() {
+	chaincode, err := contractapi.NewChaincode(&Patient{})
+	if err != nil {
+		fmt.Printf("Error creating PatientChaincode: %v", err)
+		return
+	}
+
+	if err := chaincode.Start(); err != nil {
+		fmt.Printf("Error starting PatientChaincode: %v", err)
+	}
+
+	fmt.Printf("Se chegou aqui então correu bem e foi lançado corretamente")
+}
