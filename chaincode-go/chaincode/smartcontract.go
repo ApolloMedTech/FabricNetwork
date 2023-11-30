d0ff05863cedd27f8042b44447d773e463f230f4
Alexandre Monteiro <125086148+DarkMatterAlchemist@users.noreply.github.com>
2023-11-30 12:04:31 +0000

    Nova estruttura.

--- chaincode-go/chaincode/smartcontract.go
+++ chaincode-go/chaincode/smartcontract.go
@@ -96,18 +96,3 @@ func GenerateUniqueID(socialSecurityNumber string) string {
 
 	return hashString
 }
-
-// Método de start quando o chaincode leva deploy.
-func main() {
-	chaincode, err := contractapi.NewChaincode(&Patient{})
-	if err != nil {
-		fmt.Printf("Error creating PatientChaincode: %v", err)
-		return
-	}
-
-	if err := chaincode.Start(); err != nil {
-		fmt.Printf("Error starting PatientChaincode: %v", err)
-	}
-
-	fmt.Printf("Se chegou aqui então correu bem e foi lançado corretamente")
-}
