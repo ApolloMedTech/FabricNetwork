61833fdc7beac930bf09c9bf3277a0e5acdadbed
Alexandre Monteiro <125086148+DarkMatterAlchemist@users.noreply.github.com>
2023-12-05 19:28:03 +0000

    Nova estrutura de chaincode.

--- chaincode-go/chaincode/smartcontract.go
+++ chaincode-go/chaincode/smartcontract.go
@@ -14,53 +14,43 @@ type Patient struct {
 	contractapi.Contract
 }
 
+type PatientWallet struct {
+	HealthRecords []HealthRecord `json:"healthRecords"`
+}
+
 type HealthRecord struct {
 	Description string    `json:"description"`
+	CreatedDate time.Time `json:"createDate"`
 	Date        time.Time `json:"date"`
+	EntityName  string    `json:"entityName"`
+	RecordType  string    `json:"type"`
 }
 
 func (c *Patient) AddDataToWallet(ctx contractapi.TransactionContextInterface,
-	content string, socialSecurityNumber string) error {
+	content, socialSecurityNumber, entityName, recordType string, date time.Time) error {
 
-	compositeKey, err := ctx.GetStub().CreateCompositeKey("HealthRecord", []string{"socialSecurityNumber", socialSecurityNumber})
+	compositeKey, err := createCompositeKey(ctx, socialSecurityNumber)
 	if err != nil {
 		return fmt.Errorf("failed to create composite key: %v", err)
 	}
 
-	walletBytes, err := ctx.GetStub().GetState(compositeKey)
+	patientWallet, err := getCurrentPatientWallet(ctx, compositeKey)
 	if err != nil {
-		return fmt.Errorf("failed to read patient wallet: %v", err)
-	}
-
-	var patientWallet struct {
-		HealthRecords []HealthRecord `json:"healthRecords"`
-	}
-
-	if walletBytes != nil {
-		err = json.Unmarshal(walletBytes, &patientWallet)
-		if err != nil {
-			return fmt.Errorf("failed to unmarshal patient wallet: %v", err)
-		}
-	} else {
-		// Initialize the HealthRecords slice if the wallet is empty
-		patientWallet.HealthRecords = []HealthRecord{}
+		return fmt.Errorf("failed to unmarshal patient wallet: %v", err)
 	}
 
 	newRecord := HealthRecord{
 		Description: content,
-		Date:        time.Now(),
+		CreatedDate: time.Now(),
+		Date:        date,
+		EntityName:  entityName,
+		RecordType:  recordType,
 	}
 
 	patientWallet.HealthRecords = append(patientWallet.HealthRecords, newRecord)
 
-	updatedWalletBytes, err := json.Marshal(patientWallet)
-	if err != nil {
-		return fmt.Errorf("failed to marshal updated patient wallet: %v", err)
-	}
-
-	err = ctx.GetStub().PutState(compositeKey, updatedWalletBytes)
-	if err != nil {
-		return fmt.Errorf("failed to update patient wallet: %v", err)
+	if err := updateWallet(ctx, patientWallet, compositeKey); err != nil {
+		return fmt.Errorf("failed to update the wallet: %v", err)
 	}
 
 	return nil
@@ -70,30 +60,61 @@ func (c *Patient) AddDataToWallet(ctx contractapi.TransactionContextInterface,
 func (c *Patient) GetMedicalHistory(ctx contractapi.TransactionContextInterface,
 	socialSecurityNumber string) ([]HealthRecord, error) {
 
-	compositeKey, err := ctx.GetStub().CreateCompositeKey("HealthRecord", []string{"socialSecurityNumber", socialSecurityNumber})
+	compositeKey, err := createCompositeKey(ctx, socialSecurityNumber)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create composite key: %v", err)
 	}
 
-	walletBytes, err := ctx.GetStub().GetState(compositeKey)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read patient wallet: %v", err)
-	}
-
-	if walletBytes == nil {
-		return nil, fmt.Errorf("patient wallet not found")
-	}
-
-	var patientWallet struct {
-		HealthRecords []HealthRecord `json:"healthRecords"`
-	}
-
-	err = json.Unmarshal(walletBytes, &patientWallet)
+	patientWallet, err := getCurrentPatientWallet(ctx, compositeKey)
 	if err != nil {
 		return nil, fmt.Errorf("failed to unmarshal patient wallet: %v", err)
 	}
 
 	return patientWallet.HealthRecords, nil
+}
+
+func createCompositeKey(ctx contractapi.TransactionContextInterface, key string) (string, error) {
+	compositeKey, err := ctx.GetStub().CreateCompositeKey("HealthRecord", []string{"socialSecurityNumber", key})
+	if err != nil {
+		return "", fmt.Errorf("failed to create composite key: %v", err)
+	}
+	return compositeKey, nil
+}
+
+func getCurrentPatientWallet(ctx contractapi.TransactionContextInterface,
+	key string) (*PatientWallet, error) {
+
+	walletBytes, err := ctx.GetStub().GetState(key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read patient wallet: %v", err)
+	}
+
+	var patientWallet PatientWallet
+
+	if walletBytes != nil {
+		err = json.Unmarshal(walletBytes, &patientWallet)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal patient wallet: %v", err)
+		}
+	} else {
+		patientWallet.HealthRecords = []HealthRecord{}
+	}
+
+	return &patientWallet, nil
+}
+
+func updateWallet(ctx contractapi.TransactionContextInterface, patientWallet *PatientWallet, key string) error {
+	updatedWalletBytes, err := json.Marshal(&patientWallet)
+	if err != nil {
+		return fmt.Errorf("failed to marshal updated patient wallet: %v", err)
+	}
+
+	err = ctx.GetStub().PutState(key, updatedWalletBytes)
+	if err != nil {
+		return fmt.Errorf("failed to update patient wallet: %v", err)
+	}
+
+	return nil
 }
 
 func GenerateUniqueID(socialSecurityNumber string) string {
