a463d9f98883e940fd8215665a6ac6057c958925
Alexandre Monteiro <125086148+DarkMatterAlchemist@users.noreply.github.com>
2024-04-29 19:21:59 +0100

    Update patientManagement.go

--- chaincode-go/patientManagement.go
+++ chaincode-go/patientManagement.go
@@ -3,19 +3,20 @@ package main
 import (
 	"fmt"
 
+	"github.com/ApolloMedTech/FabricNetwork/chaincode-go/chaincode"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
 // Método de start quando o chaincode leva deploy.
 func main() {
-	assetChaincode, err := contractapi.NewChaincode(&Patient{})
+	assetChaincode, err := contractapi.NewChaincode(&chaincode.HealthContract{})
 	if err != nil {
-		fmt.Printf("Error creating PatientChaincode: %v", err)
+		fmt.Printf("Error creating HealthContract: %v", err)
 		return
 	}
 
 	if err := assetChaincode.Start(); err != nil {
-		fmt.Printf("Error starting PatientChaincode: %v", err)
+		fmt.Printf("Error starting HealthContract: %v", err)
 	}
 
 	fmt.Printf("Se chegou aqui então correu bem e foi lançado corretamente")
